59205b97fe191eb71465260b5abf077929ed27bd
cagridogan08 <cagridogan08@gmail.com>
2022-08-16 21:26:15 +0300

    Local month names on Chart

--- src/components/Expenses/ExpensesChart.js
+++ src/components/Expenses/ExpensesChart.js
@@ -20,6 +20,7 @@ const ExpensesChart = (props) => {
 
   for (const expense of props.expenses) {
     const expenseMonth = expense.date.getMonth(); // starting at 0 => January => 0
+    chartDataPoints[expenseMonth].label=expense.date.toLocaleString('tr-TR',{month:'long'});
     chartDataPoints[expenseMonth].value += expense.amount;
   }
 
